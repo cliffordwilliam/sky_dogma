e0642472fe5f179961f10fb1d25740d31ccb1663
Clifford William <ccliffordwilliam@gmail.com>
2023-10-20 16:47:17 +0800

    Feat(camera and scrolling background): - Everything is now drawn in relative to camera global position - Camera can follow anything - Scrolling background scrolls indefinitely

--- test.py
+++ test.py
@@ -1,14 +1,25 @@
 import pygame as pg  # https://pyga.me/docs/
 import os
 
+"""
+Some motivational words for myself:
+This is going to be the best top down shooter in python ever!
+This framework is going to be reusable for other 2D games also.
+"""
+
 ################
 # GLOBAL CONST #
 ################
 FPS = 60
 DISPLAY_SIZE = (1280, 720)  # use the user setting to define this
 NATIVE_RESOLUTION = (320, 180)
+HALF_NATIVE_RESOLUTION = (160, 90)
 PNG_DIR = "assets/png"
-PNG_DICT = {}
+SURFACES_DICT = {}
+BACKGROUND_WIDTH = 336
+HALF_BACKGROUND_WIDTH = 168
+ONE_TILE = 16
+
 
 ###########
 # PG INIT #
@@ -18,6 +29,7 @@ DISPLAY_SURFACE = pg.display.set_mode(DISPLAY_SIZE)
 CLOCK = pg.time.Clock()
 
 is_running = True
+
 
 ##########
 # CANVAS #
@@ -36,7 +48,7 @@ for filename in os.listdir(PNG_DIR):
 
     # key = filename (without extension) | val = surface
     key = os.path.splitext(filename)[0]
-    PNG_DICT[key] = SURFACE
+    SURFACES_DICT[key] = SURFACE
 
 
 ##########
@@ -61,6 +73,28 @@ def Sign(value):
         return -1
     else:
         return 0
+
+def apply_flash_shader(surface, color=(0, 0, 0, 255)):
+    """
+    Apply a flash shader to given surface by blending it with a given color.
+    """
+    # copy surface (so does not change original)
+    surface_copy = surface.copy()
+
+    # check each pixel, turn non-transparent pixels to given color
+    for y in range(surface_copy.get_height()):
+        for x in range(surface_copy.get_width()):
+            pixel = surface_copy.get_at((x, y))
+            # pixel is non-transparent? set color
+            if pixel[3] != 0:
+                surface_copy.set_at((x, y), color)
+
+    # blit surface copy to output surface
+    output_surface = pg.Surface(surface_copy.get_size(), pg.SRCALPHA)
+    output_surface.blit(surface_copy, (0, 0))
+
+    return output_surface
+
 
 ########
 # MISC #
@@ -104,6 +138,61 @@ class Input:
 Input = Input()
 
 
+class Camera(pg.sprite.Sprite):
+    """
+    A point in space that can follow a target, things are drawn based on camera global position.
+    Camera is an illusion where things are drawn in respect to its global position.
+    This game camera only move sideways, left limit is always 0. Right limit is always 16 (bg is always 336 wide)
+    """
+    def __init__(self):
+        super().__init__()
+        ##############
+        # PROPERTIES #
+        ##############
+        # just so that this counts as a sprite obj, so that it can be added to sprite group (for the updates)
+        self.image = None
+        self.rect = None
+
+        self.global_position = pg.Vector2(0, 0)
+        self.target = None
+        self.MOVEMENT_WEIGHT = 0.1
+        self.RIGHT_LIMIT = BACKGROUND_WIDTH - NATIVE_RESOLUTION[0]
+
+    ###########
+    # METHODS #
+    ###########
+    def update(self, delta):
+        """
+        Updates global position to approach target.
+        """
+        if self.target == None:
+            return
+        
+        # this game camera do not follow vertical movements
+        # centered_target_y = self.target.rect.y - HALF_NATIVE_RESOLUTION[1]
+        centered_target_x = self.target.rect.x - HALF_NATIVE_RESOLUTION[0]
+        
+        # this game camera do not follow vertical movements
+        # self.global_position.y = lerp(self.global_position.y, centered_target_y, self.MOVEMENT_WEIGHT)
+        self.global_position.x = lerp(self.global_position.x, centered_target_x, self.MOVEMENT_WEIGHT)
+
+        # camera limit
+        self.global_position.x = max(0, min(self.global_position.x, self.RIGHT_LIMIT))
+
+    ##########
+    # SETTER #
+    ##########
+    def set_target(self, target: any):
+        self.target = target
+        centered_target_x = self.target.rect.x - HALF_NATIVE_RESOLUTION[0]
+        self.global_position.x = centered_target_x
+
+
+# global class for sprite class to use for drawing
+# sprite class is everywhere and they all need the camera
+Cam = Camera()
+
+
 # for rendering & collision
 class Group(pg.sprite.Group):
     """
@@ -116,9 +205,12 @@ class Group(pg.sprite.Group):
         """
         Override the default draw method to instead call the actor draw method, 
         actor draw method needs the frame index data to draw certain section of their spritesheet.
+        Also for camera = offset where to draw things based on player position
         """
         for spr in self.sprites():
+            # get player offset
             spr.draw()
+
 
 #########
 # NODES #
@@ -186,7 +278,6 @@ class Animator:
         else:
             self.listeners[event_name] = [method]
 
-    # needs to be called!
     def update(self):
         """
         Needs to be called, updates the elapsed_frame. Which is used to set attribute and make events happen.
@@ -256,14 +347,14 @@ class Sprite(pg.sprite.Sprite):
         self.frame_data = {}
 
         # set frame data
-        frame_width = surface.get_width() // h_frame
-        frame_height = surface.get_height() // v_frame
+        self.frame_width = surface.get_width() // h_frame
+        self.frame_height = surface.get_height() // v_frame
 
         for col in range(h_frame):
             for row in range(v_frame):
-                frame_x = col * frame_width
-                frame_y = row * frame_height
-                self.frame_data[len(self.frame_data)] = (frame_x, frame_y, frame_width, frame_height)
+                frame_x = col * self.frame_width
+                frame_y = row * self.frame_height
+                self.frame_data[len(self.frame_data)] = (frame_x, frame_y, self.frame_width, self.frame_height)
     
     ###########
     # METHODS #
@@ -272,14 +363,141 @@ class Sprite(pg.sprite.Sprite):
         """
         Frame property uses the frame data to determine what part of the sprite should it blit to NATIVE_SURFACE.
         """
+        # get the sprite from spritesheet
         frame_x, frame_y, frame_width, frame_height = self.frame_data[self.frame]
         frame_rect = pg.Rect(frame_x, frame_y, frame_width, frame_height)
-        NATIVE_SURFACE.blit(self.image, self.rect, frame_rect)
+
+        # global position -> position in respect to camera
+        on_camera_rect = pg.Rect(
+            self.rect.x - Cam.global_position.x,
+            self.rect.y - Cam.global_position.y,
+            self.rect.width,
+            self.rect.height
+        )
+
+        NATIVE_SURFACE.blit(self.image, on_camera_rect, frame_rect)
 
 
 ##########
 # ACTORS #
 ##########
+class Background(pg.sprite.Sprite):
+    """
+    A background sprite.
+    """
+    def __init__(self):
+        super().__init__()
+        ##############   
+        # PROPERTIES #
+        ##############
+        self.Sprite = Sprite(surface=SURFACES_DICT["field"], h_frame=1, v_frame=1)
+        self.image = self.Sprite.image
+        self.rect = self.Sprite.rect
+
+    ###########
+    # METHODS #
+    ###########
+    def draw(self):
+        """
+        This func is called by parent. 
+        Call sprite objects draw functions here.
+        """
+        self.Sprite.draw()
+
+
+class BackgroundScroller(pg.sprite.Sprite):
+    """
+    Scrolls its 2 background children.
+    This allows for smooth transitions (change the background children surfaces)
+    """
+    def __init__(self):
+        super().__init__()
+        ############
+        # CHILDREN #
+        ############
+        self.BackgroundTop = Background()
+        self.BackgroundBottom = Background()
+
+        # position top bg bottom to be at bottom background top
+        self.BackgroundTop.rect.y -= self.BackgroundTop.rect.height
+
+    ###########
+    # METHODS #
+    ###########
+    def draw(self):
+        """
+        This func is called by the Group class. 
+        Call sprite objects draw functions here.
+        """
+        # background top
+        self.BackgroundTop.draw()
+
+        # background bottom
+        self.BackgroundBottom.draw()
+    
+    
+    def update(self, delta):
+        """
+        This func is called by the Group class.
+        Updates background children position.
+        """
+        # background top
+        self.BackgroundTop.rect.y += 1
+        if self.BackgroundTop.rect.y == self.BackgroundTop.rect.height:
+            self.BackgroundTop.rect.bottom = 0
+
+        # background bottom
+        self.BackgroundBottom.rect.y += 1
+        if self.BackgroundBottom.rect.y == self.BackgroundBottom.rect.height:
+            self.BackgroundBottom.rect.bottom = 0
+    
+    # TODO: add change background surface method, and handle smooth surface transition
+
+
+class PlayerShadow(pg.sprite.Sprite):
+    """
+    This is always a child of someone. Frame is updated based on parent's. 
+    """
+    def __init__(self):
+        super().__init__()
+        ##############   
+        # PROPERTIES #
+        ##############
+
+        # pre process the surface, turn non transparent pixel to black
+        black_sprite_sheet_surface = apply_flash_shader(SURFACES_DICT["player"], color=(0, 0, 0, 255))
+
+        # scale it down by 50%
+        # TODO: have this scaling be configurable for animation (take off and landing)
+        half_width = black_sprite_sheet_surface.get_width() // 2
+        half_height = black_sprite_sheet_surface.get_height() // 2
+        scaled_black_sprite_sheet_surface = pg.transform.scale(black_sprite_sheet_surface, (half_width, half_height))
+
+        self.Sprite = Sprite(surface=scaled_black_sprite_sheet_surface, h_frame=11, v_frame=1)
+        self.image = self.Sprite.image
+        self.rect = self.Sprite.rect
+
+        self.local_position = pg.Vector2(0, 0)
+
+    ###########
+    # METHODS #
+    ###########
+    def draw(self):
+        """
+        This func is called by the parent.
+        """
+        self.Sprite.draw()
+    
+    def update(self, delta, parent_rect, parent_sprite_frame_index):
+        """
+        This func is called by the parent.
+        Updates position and my Sprite frame index.
+        """
+        self.rect.x = parent_rect.x + self.local_position.x
+        self.rect.y = parent_rect.y + self.local_position.y
+        self.Sprite.frame = parent_sprite_frame_index
+
+
 class PlayerExhaustFlame(pg.sprite.Sprite):
     """
     This is always a child of someone. Autoplays its burning animation. 
@@ -290,7 +508,7 @@ class PlayerExhaustFlame(pg.sprite.Sprite):
         ##############   
         # PROPERTIES #
         ##############
-        self.Sprite = Sprite(surface=PNG_DICT["player_exhaust"], h_frame=3, v_frame=1)
+        self.Sprite = Sprite(surface=SURFACES_DICT["player_exhaust"], h_frame=3, v_frame=1)
         self.image = self.Sprite.image
         self.rect = self.Sprite.rect
 
@@ -323,6 +541,7 @@ class PlayerExhaustFlame(pg.sprite.Sprite):
     
     def update(self, delta, parent_rect):
         """
+        This func is called by the parent.
         Updates animation and position.
         """
         self.animator.update()
@@ -340,16 +559,28 @@ class Player(pg.sprite.Sprite):
         ##############   
         # PROPERTIES #
         ##############
-        self.Sprite = Sprite(surface=PNG_DICT["player"], h_frame=11, v_frame=1)
+        self.Sprite = Sprite(surface=SURFACES_DICT["player"], h_frame=11, v_frame=1)
         self.image = self.Sprite.image
         self.rect = self.Sprite.rect
 
         # initial sprite frame (5 = no banking)
         self.Sprite.frame = 5
 
-        # children (create a list to iter each children later)
+        ############
+        # CHILDREN #
+        ############
+        # exhaust flame
         self.ExhaustFlame = PlayerExhaustFlame()
         self.ExhaustFlame.local_position.y = 15.0
+        # shadow
+        self.Shadow = PlayerShadow()
+        self.Shadow.local_position.y = 15.0
+        self.Shadow.local_position.x = 15.0
+        # lists (drawing does not need any args)
+        self.children = [
+            self.ExhaustFlame,
+            self.Shadow
+        ]
 
         # movement
         self.MAX_VELOCITY = 90.0  # px / s
@@ -366,9 +597,14 @@ class Player(pg.sprite.Sprite):
         Call sprite objects draw functions here.
         """
         self.Sprite.draw()
+
+        # draw children
+        for child in self.children:
+            child.draw()
     
     def update(self, delta):
         """
+        This func is called by the Group class.
         Updates anything here based on user input / other actor influences.
         """
         # direction
@@ -395,6 +631,8 @@ class Player(pg.sprite.Sprite):
 
         # update children (position, animation, etc)
         self.ExhaustFlame.update(delta, self.rect)
+        self.Shadow.update(delta, self.rect, self.Sprite.frame)
+        
     
     ##########
     # HELPER #
@@ -437,6 +675,7 @@ class Player(pg.sprite.Sprite):
             self.rect.y += sign
             move -= sign
 
+
 ##########
 # SCENES #
 ##########
@@ -448,16 +687,35 @@ class Test:
         ############
         # CHILDREN #
         ############
+        # SETUP PLAYER
         self.Player = Player()
+        # in this game the player always start like this, bg is wider but has same height as window
+        # update player position to middle
+        self.Player.rect.topleft = pg.Vector2(HALF_BACKGROUND_WIDTH, HALF_NATIVE_RESOLUTION[1])
+        self.Player.rect.top -= self.Player.Sprite.frame_height / 2
+        # shift downward by 3 tiles - 1 tile is 16px
+        self.Player.rect.top += ONE_TILE * 3
+
+        # background scroller
+        self.BackgroundScroller = BackgroundScroller()
+
+        # SETUP CAMERA
+        # camera initial target in player
+        Cam.set_target(self.Player)
+        # no need to update camera limit, this game camera limit is fixed
 
         # layers (can do quadtree collision AABB!)
         self.DrawnLayer = Group()  # for things that needs to be drawn
         self.UpdateLayer = Group()  # for things that needs to be updated
 
-        # fill layers (order matters, top = drawn most bottom)
-        self.DrawnLayer.add(self.Player.ExhaustFlame)
+        # fill draw layers (order matters, top = drawn most bottom)
+        self.DrawnLayer.add(self.BackgroundScroller)
         self.DrawnLayer.add(self.Player)
+
+        # fill update layers, anything that needs updating goes here
+        self.UpdateLayer.add(self.BackgroundScroller)
         self.UpdateLayer.add(self.Player)
+        self.UpdateLayer.add(Cam)
     
     ###########
     # METHODS #
@@ -500,6 +758,11 @@ while is_running:
 
     # DRAW
     scene.draw()
+
+    # DEBUG
+    # draw on viewport, 2 lines in mid horizontal and mid vertical
+    # pg.draw.line(NATIVE_SURFACE, "red", (167, 0), (167, 320), 2)
+    # pg.draw.line(NATIVE_SURFACE, "red", (0, 90), (335, 90), 2)
 
     # BLIT NATIVE TO DISPLAY
     SCALED_NATIVE_SURFACE = pg.transform.scale(NATIVE_SURFACE, DISPLAY_SIZE)
